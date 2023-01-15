73d6f3882ae7136daadc058c8dca8a6906a9b8ca
Erich Queiroz <95364468+erich-queiroz@users.noreply.github.com>
2023-01-15 16:28:10 -0300

    Atualizando JS
    Melhoria de lógica sobre validações de inputs e mensagens sobre o resultado do IMC.

--- script.js
+++ script.js
@@ -77,24 +77,34 @@ function calculeImc() {
         peso.style.border = "none";
     
 let msgPeso = document.getElementById('mensagemPeso');
+const label = document.getElementsByClassName('alturaLabel');
 
-
+   
     if(!peso.value || !altura.value) {
-        msgErro.innerHTML = 'Preencha todos os campos!'
-    }  else {
+        msgErro.innerHTML = 'Preencha os campos';
+        if(!altura.value) {
+            label.innerHTML = label + '*'
+            altura.style.border = '1px solid red';
+            msgErro.innerHTML = 'Digite a altura';
+            } else if (!peso.value) {
+            peso.style.border = '2px solid red';    
+            msgErro.innerHTML = 'Digite o peso';
+        }
+        
+    }  else  {
         let imc  = (peso.value) / (altura.value * altura.value);
         msgErro.innerHTML = 'Seu IMC é ' + Math.round(imc);
 
         if(imc <= 18.5){
             msgPeso.innerHTML = "Magreza. Você está <b>abaixo do peso</b>";
-        } else if (imc > 185 <= 24.9) {
+        } else if (imc > 18.5 && imc <= 24.9) {
             msgPeso.innerHTML = "Parabéns! Você está com o <b>peso ideal</b>!";
-        } else if (imc > 24.9 <= 30) {
+        } else if (imc > 24.9 && imc <= 30) {
             msgPeso.innerHTML = "Fique de olho! Você está com <b>sobrepeso</b>!";
         } else {
             msgPeso.innerHTML = "Cuidado!!! Você está com obesidade!"
         }
-
+    
         console.log(imc);
     }
     
